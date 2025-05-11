9608d864ff14e2f0500bb9b5a608a6383f176e2f
Bereket Tesfaye <tesfayebereket909@gmail.com>
2025-05-11 23:34:18 +0300

    Update system.h
    rent car funcition added

--- system.h
+++ system.h
@@ -39,7 +39,68 @@ class System {
 /*ASTER*/
   
   
-  /*BEREKET*/
+//BEREKET
+  int rentCar(){
+   try {
+    std::string name;
+    std::string email;
+    std::string made;
+    std::string model;
+    int affectedRows;
+    int year;
+     
+    std::cout << "\nEnter your name: ";
+    std::getline(std::cin,name);
+    std::cout << "\nEnter your email: ";
+    std::getline(std::cin, email);
+    std::cout << "\nEnter Car Brand: ";
+    std::getline(std::cin, made);
+    std::cout << "\nEnter The Model of the the Car " << made << ": ";
+    std::getline(std::cin, model);
+    std::cout << "\nEnter manufacture Year For car " << made << " " << model << ": " ;
+    std::cin>> year;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    
+    std::unique_ptr<PreparedStatement> pstmt(conn->prepareStatement("SELECT id FROM Customers WHERE name= ? AND email = ? "));
+    pstmt->setString(1,name);
+    pstmt-> setString(2,email);
+    std::unique_ptr<ResultSet> res (pstmt->executeQuery());
+    int userId;
+    
+    if (res->next()){
+     userId = res->getInt("id");
+     } else {
+     std::cout << "\nNo User With a given username and email. Please goto registration page.\n";
+     return 0;
+    }
+   
+    pstmt.reset(conn->prepareStatement("SELECT id FROM Cars WHERE made = ? AND model = ? AND year = ? AND isAvaileble = 1"));
+    pstmt->setString(1,made);
+    pstmt->setString(2,model);
+    pstmt->setInt(3,year);
+    res.reset(pstmt->executeQuery());
+    int carId;
+    
+    if (res->next()){
+     carId = res->getInt("id");
+    }else{
+    std::cout << "The car with the given atributes doesnt exist please check out list of available cars!\n" <<std::endl;
+    affectedRows =0;
+    return 0;
+    
+    }
+  
+   pstmt.reset(conn->prepareStatement("UPDATE Cars SET rentedBy = ? ,rental_date = CURRENT_TIMESTAMP ,isAvaileble = 0 WHERE id = ?"));
+   pstmt->setInt(1,userId);
+   pstmt->setInt(2,carId);
+   affectedRows = pstmt->executeUpdate();
+   std::cout << (affectedRows > 0? "You have successfuly rented the car !\n" : "Someting goes wrong while updateing the car date!\n"); 
+   }catch (SQLException &e){
+   std::cout << "Error: " << e.what() << std::endl;
+  } 
+  return 0;
+  
+  }
   
  /*DAGMAWI*/
 
