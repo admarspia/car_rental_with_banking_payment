577ca74b767b95b16e7f0c7630b78a87169b98c9
Bereket Tesfaye <tesfayebereket909@gmail.com>
2025-05-12 00:22:46 +0300

    Update banksys.h
    added transfer funcition

--- banksys.h
+++ banksys.h
@@ -91,7 +91,105 @@ class BankSystem {
 
  
 
- /*ABENEZER*/
+ //ABENEZER
+ int transfer(){
+  try {
+   int accofsender;
+   int accofrecipant;
+   double recipantbalance;
+   double senderbalance;
+   int amount;
+   int affectedRows;
+   std::string hashed;
+   std::string password;
+   std::string description ;
+   std::string transaction_type = "transfered";
+
+   
+   std::cout<< "Enter your account number: ";
+   std::cin>>accofsender;
+   std::cout<< "Enter the account of the recipant: ";
+   std::cin>> accofrecipant;
+   std::cout << "Enter the amount to transfer: ";
+   std::cin>> amount;
+   
+   std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+   std::cout <<"Enter description : ";
+   std::getline(std::cin,description);
+   
+   std::cout <<"Enter Password : ";
+   std::getline(std::cin,password);
+   
+   std::unique_ptr<PreparedStatement> pstmt(conn->prepareStatement("SELECT password, acount_number, balance FROM users WHERE acount_number  IN (?,?) "));
+   pstmt->setInt(1,accofsender);
+   pstmt->setInt(2,accofrecipant);
+   
+   std::unique_ptr<ResultSet> res(pstmt->executeQuery());
+   int actors = 0;
+   while (res->next()){
+    int accnum = res->getInt("acount_number");
+    int bal = res->getDouble("balance");
+
+    if (accnum == accofsender){
+     senderbalance = bal;
+     hashed = res->getString("password");
+    }else {
+     recipantbalance = bal;
+    } 
+    actors+=1;
+   }
+   if (actors !=2){
+     std::cout << "acount not found!.";
+     return 0;
+   }
+   if (senderbalance >= amount){
+    senderbalance -= amount;
+    recipantbalance +=amount;
+   }else {
+    std::cout << "inseficent balance!.\n";
+    return 0;
+   }
+   if (!validatePassword(hashed,password)) { 
+    std::cout<< "Incorrect Password!" <<std::endl;
+    return 0;
+   }
+  
+   pstmt.reset(conn->prepareStatement("UPDATE users SET balance = ? WHERE  acount_number = ?"));
+   pstmt->setDouble(1,senderbalance);
+   pstmt->setInt(2,accofsender);
+   affectedRows = pstmt->executeUpdate();
+   
+   pstmt.reset(conn->prepareStatement("UPDATE users SET balance = ? WHERE  acount_number = ?"));
+   pstmt->setDouble(1,recipantbalance);
+   pstmt->setInt(2,accofrecipant);
+   affectedRows = pstmt->executeUpdate();
+   
+   if (affectedRows != 0){
+   pstmt.reset(conn->prepareStatement("INSERT INTO Transaction (user_id,transaction_type,amount,description) VALUES(?,?,?,?)"));
+   pstmt->setInt(1,accofsender);
+   pstmt->setString(2,"transfered");
+   pstmt->setDouble(3,amount);
+   pstmt->setString(4,description);
+   affectedRows = pstmt->executeUpdate();
+   //reciver side
+   pstmt.reset(conn->prepareStatement("INSERT INTO Transaction (user_id,transaction_type,amount,description) VALUES(?,?,?,?)"));
+   pstmt->setInt(1,accofrecipant);
+   pstmt->setString(2,"credited");
+   pstmt->setDouble(3,amount);
+   pstmt->setString(4,description);
+   
+   affectedRows = pstmt->executeUpdate();
+   std::cout<< (affectedRows !=0? "Done!.\n":"Something went Wrong! recording the transaction.\n") ;
+   std::cout << "Transfer  Succeeded!.\n";
+   } else {
+       std::cout<< "Something went Wrong!.\n" ;
+   }
+
+  } catch (SQLException & e){
+     std::cout<< "Error: " <<  e.what() << std::endl;
+  } 
+  return 0;
+ }
 
  
 
